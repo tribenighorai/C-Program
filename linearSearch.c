05a44c8f5b32895d25c09c6b6523f4d1a203af52
tribenighorai <tapasghori9@gmail.com>
2026-04-23 18:35:15 +0530

    Create linearSearch.c

---
+++ linearSearch.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key)
+            return i; 
+    }
+    return -1; 
+}
+
+int main() {
+    int arr[] = {10, 25, 30, 45, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 45;
+
+    int result = linearSearch(arr, n, key);
+
+    if (result == -1)
+        printf("Element not found\n");
+    else
+        printf("Element found at index: %d\n", result);
+
+    return 0;
+}
