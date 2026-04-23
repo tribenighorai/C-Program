d95fd1abfd61c16006d0b572784bc5af54b85017
tribenighorai <tapasghori9@gmail.com>
2026-04-23 19:14:17 +0530

    Create 01Knapsack.c

---
+++ 01Knapsack.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+int knapsack(int W, int wt[], int val[], int n) {
+    int dp[n+1][W+1];
+
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if (wt[i-1] <= w)
+                dp[i][w] = max(val[i-1] + dp[i-1][w - wt[i-1]], dp[i-1][w]);
+            else
+                dp[i][w] = dp[i-1][w];
+        }
+    }
+    return dp[n][W];
+}
+
+int main() {
+    int n, W;
+    printf("Enter number of items:");
+    scanf("%d", &n);
+
+    int val[n], wt[n];
+    printf("Enter values and weights of items:");
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &val[i], &wt[i]);
+    }
+
+    printf("Enter capacity of knapsack:");
+    scanf("%d", &W);
+
+    printf("Maximum value in Knapsack = %d\n", knapsack(W, wt, val, n));
+    return 0;
+}
